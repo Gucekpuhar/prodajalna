e7821ceb34c75035522fce3c7d04aef69282c249
gucekpuhar <gucekenej@gmail.com>
2016-05-05 16:55:33 +0000

    Racun se izpis, potrebno se nekaj popravkov

--- eracun.js
+++ eracun.js
@@ -133,7 +133,7 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+      callback(napaka, vrstice);
     })
 }
 
@@ -142,13 +142,27 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      console.log(vrstice);
+     callback(napaka, vrstice);
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
-streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
+streznik.get('/izpisiRacunBaza/:idRacuna', function(zahteva, odgovor) { 
+  var TrenutnaStranka = zahteva.query.seznamRacunov; 
+  var TrenutniRacun = TrenutnaStranka;
+  
+  strankaIzRacuna(TrenutnaStranka, function(napaka1, stranka) { 
+    pesmiIzRacuna(TrenutniRacun, function(napaka2, pesmi) { 
+      odgovor.setHeader('content-type', 'text/xml'); 
+      odgovor.render('eslog', { 
+        vizualiziraj: true, 
+        postavkeRacuna: pesmi 
+  
+         });
+    
+      
+    });
+  });
 })
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
